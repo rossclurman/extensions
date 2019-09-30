5f8e26f9dc23515ca3a5e382ca556195bb69ecfd
singlethreaded <chris@infocyte.com>
2019-09-30 21:52:45 +0100

    template

---
+++ examples/template.lua
@@ -0,0 +1,46 @@
+--[[
+	Infocyte Extension
+	Name: Template
+	Type: Collection
+	Description: Collects additional data
+	Author: Infocyte
+	Created: 9-19-2019
+	Updated: 9-19-2019 (Gerritz)
+]]--
+
+----------------------------------------------------
+-- SECTION 1: Variables & Functions
+----------------------------------------------------
+OS = hunt.env.os()
+
+
+----------------------------------------------------
+-- SECTION 2: Collection or Actions
+----------------------------------------------------
+
+if string.find(OS, "windows") then
+  -- Insert your Windows Code
+
+elseif string.find(OS, "osx") then
+	-- Insert your MacOS Code
+
+else
+	-- Insert your POSIX (linux) Code
+
+end
+
+----------------------------------------------------
+-- SECTION 3: Analysis
+--    Host-side processing and analysis can be
+--    written here.
+----------------------------------------------------
+
+
+----------------------------------------------------
+-- SECTION 4: Output / Results
+--    Set the threat status of the overall script.
+--    Send any data in text using one or more
+--    log statements.
+----------------------------------------------------
+hunt.log("Extension successfully executed on "..computername)
+hunt.setthreatstatus("Good")
